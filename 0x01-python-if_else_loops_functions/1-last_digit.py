4e3319b1a126036829a55a1417790dd4e0eaf5f5
IVashty <vashtylavilavi50@gmail.com>
2022-09-24 08:33:36 +0000

    Task 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,16 +4,8 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10 if number > 0 else int(repr(number)[-1]) * -1
 if last_digit > 5:
-    print(
-        "last digit of {} is {} and is greater than 5".format(
-            number, last_digit
-        )
-    )
+    print("last digit of {} is {} and is greater than 5".format(number, last_digit))
 elif last_digit == 0:
     print("last digit {} is {} is 0".format(number, last_digit))
 elif last_digit < 6:
-    print(
-        "last digit is {} is {} is less than 6 and not 0".format(
-            number, last_digit
-        )
-    )
+    print("last digit is {} is {} is less than 6 and not 0".format(number, last_digit))
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -11,4 +11,9 @@
             - if the number is less than 0: is negative
                 followed by a new line
 1. This program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable number.
-
+2. Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.
+    - You can only use one print function with string format
+   - You can only use one loop in your code
+   - You are not allowed to store characters in a variable
+   - You are not allowed to import any module
+3
