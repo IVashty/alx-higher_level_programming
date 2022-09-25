e0d66d3e90f9f7e4b15cbb0ff184abdc3efdea7f
IVashty <vashtylavilavi50@gmail.com>
2022-09-25 15:02:48 +0000

    Task 8 update 1

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     alist = list(str)
-    for v in range(len(str)):
-        if ord(alist[v]) <= 122 and ord(alist[v]) >= 97:
-            alist[v] = chr(ord(alist[v]) - 32)
-        else:
-            alist = 0
+    for v in range(len(alist)):
+        if (ord(alist[v]) <= 122 and ord(alist[v]) >= 97):
+            alist[v] = (chr(ord(alist[v]) - 32))
             print("{}".format(("").join(alist)))
-    print()
