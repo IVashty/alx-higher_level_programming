618af349a7c1bae3201465a5d74b6f60a68f5997
IVashty <vashtylavilavi50@gmail.com>
2022-09-25 21:17:24 +0000

    Task 2

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -4,3 +4,23 @@ print_list_integer = __import__("0-print_list_integer").print_list_integer
 
 my_list = [1, 2, 3, 4, 5]
 print_list_integer(my_list)
+
+#task 1
+element_at = __import__('1-element_at').element_at
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+
+# Task 2
+replace_in_list = __import__('2-replace_in_list').replace_in_list
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = replace_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
+
+
