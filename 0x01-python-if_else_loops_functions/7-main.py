276fcf862aa61151a4b7a9250d59c38d3c7dcf55
IVashty <vashtylavilavi50@gmail.com>
2022-09-25 18:42:14 +0000

    Task 12 update 3

--- 0x01-python-if_else_loops_functions/7-main.py
+++ 0x01-python-if_else_loops_functions/7-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#quiz 7
+# quiz 7
 islower = __import__("7-islower").islower
 
 print("a is {}".format("lower" if islower("a") else "upper"))
@@ -8,29 +8,29 @@ print("A is {}".format("lower" if islower("A") else "upper"))
 print("3 is {}".format("lower" if islower("3") else "upper"))
 print("g is {}".format("lower" if islower("g") else "upper"))
 
-#quiz 8
-uppercase = __import__('8-uppercase').uppercase
+# quiz 8
+uppercase = __import__("8-uppercase").uppercase
 
 uppercase("best")
 uppercase("Best School 98 Battery street")
 
-#quiz 9
-print_last_digit = __import__('9-print_last_digit').print_last_digit
+# quiz 9
+print_last_digit = __import__("9-print_last_digit").print_last_digit
 
 print_last_digit(98)
 print_last_digit(0)
 r = print_last_digit(-1024)
 print(r)
 
-#quiz 10
-add = __import__('10-add').add
+# quiz 10
+add = __import__("10-add").add
 
 print(add(1, 2))
 print(add(98, 0))
 print(add(100, -2))
 
-#quiz 11
-pow = __import__('11-pow').pow
+# quiz 11
+pow = __import__("11-pow").pow
 
 print(pow(2, 2))
 print(pow(98, 2))
@@ -38,9 +38,8 @@ print(pow(98, 0))
 print(pow(100, -2))
 print(pow(-4, 5))
 
-#quiz 12
-fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
+# quiz 12
+fizzbuzz = __import__("12-fizzbuzz").fizzbuzz
 
 fizzbuzz()
 print("")
-
