f50363af760c728e04a783fcfef2f458481930ba
IVashty <vashtylavilavi50@gmail.com>
2022-09-26 05:51:46 +0000

    Task 5 update 1

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,8 +5,7 @@ def no_c(my_string):
     new_string = my_string[:]
 
     for str in range(len(my_string)):
-        if my_string[str] == "c" or my_string[str] == "C":
-            new_string = new_string[: (str - ind)] + my_string[(str + 1) :]
+        if (my_string[str] == "c") or (my_string[str] == "C"):
+            new_string = (new_string[:(str - ind)]) + (my_string[(str + 1):])
             ind += 1
-
     return new_string
