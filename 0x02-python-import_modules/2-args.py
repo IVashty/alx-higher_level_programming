1960b4f754b52d8e0e6c7fc1e5cf9a8fa6d83a66
IVashty <vashtylavilavi50@gmail.com>
2022-10-08 13:43:13 +0000

    Task 2 corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,11 @@ if __name__ == "__main__":
     import sys
 
     addition = len(sys.argv)
-    if addition <= 1:
-        print("0 argument.")
+    if addition == 1:
+        print("{} arguments.".format(addition - 1))
+    elif addition == 2:
+        print("{} argument:".format(addition - 1))
     else:
-        if addition == 2:
-            print("{} arguments:".format(addition - 1))
-        else:
-            print("{} argument:".format(addition - 1))
-        for i in range(1, addition):
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(addition - 1))
+    for i in range(1, addition):
+        print("{}: {}".format(i, sys.argv[i]))
