41b95de5db7ebed5777d7f7bad11aee776893bd8
IVashty <vashtylavilavi50@gmail.com>
2022-10-08 14:46:20 +0000

    Task 6 update 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
+from sys import argv
+from calculator_1 import add, sub, mul, div
 
-    num_args = len(sys.argv)
-    if num_args != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+if __name__ == "__main__":
+    argc = len(argv)
+    result = 0
+    operators = ["+", "-", "*", "/"]
+
+    if argc != 4:
+        print(f"Usage: {argv[0]} <a> <operator> <b>")
         exit(1)
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
-    if op is "+":
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op is "-":
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op is "*":
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op is "/":
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
-    else:
+
+    if argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+
+    num1 = int(argv[1])
+    operator = argv[2]
+    num2 = int(argv[3])
+
+    if operator == "+":
+        result = add(num1, num2)
+    elif operator == "-":
+        result = sub(num1, num2)
+    elif operator == "*":
+        result = mul(num1, num2)
+    else:
+        result = div(num1, num2)
+    print("{:d} {:s} {:d} = {:d}".format(num1, operator, num2, result))
