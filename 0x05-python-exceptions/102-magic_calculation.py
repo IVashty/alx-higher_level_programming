292d49069af54c096ee27f12ae8651d0d1bb3fea
IVashty <vashtylavilavi50@gmail.com>
2022-10-12 07:21:26 +0000

    Task 9

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for element in range(1, 3):
+        try:
+            if element > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / element
+        except:
+            result = a + b
+            break
+    return (result)
