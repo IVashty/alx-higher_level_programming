a8fc2165915096cc46fdb7e90478297915a3198f
IVashty <vashtylavilavi50@gmail.com>
2022-10-13 20:27:48 +0000

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Super simple Square Module.
+"""
+
+
+class Square:
+    """
+    Only does one thing: Gives you its area.
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        print(
+            "\n".join(
+                ["".join(["#" for v in range(self.__size)]) for k in
+                    range(self.__size)]
+            )
+        )
