fd76b0f2b0a45168d86a3eee09c251a885e2d6c5
IVashty <vashtylavilavi50@gmail.com>
2022-10-13 19:34:24 +0000

    Tasks questions added

--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -5,7 +5,7 @@ my_square = Square()
 print(type(my_square))
 print(my_square.__dict__)
 
-#Task 1
+# Task 1
 Square = __import__('1-square').Square
 
 my_square = Square(3)
@@ -19,6 +19,41 @@ except Exception as e:
 
 try:
     print(my_square.__size)
+except Exception as e:
+    print(e)
+
+# Task 2
+Square = __import__('2-square').Square
+
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
 except Exception as e:
     print(e)
 
