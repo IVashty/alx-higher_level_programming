c7a4a0ee08889f19cc0f87a553d493553347f6ac
IVashty <vashtylavilavi50@gmail.com>
2022-10-25 20:35:17 +0000

    Task 1 error correction

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -15,5 +15,5 @@ def write_file(filename="", text=""):
 
     Returns: the number of characters written.
     """
-    with open(filename, "w+") as f:
+    with open(filename, "w+", encoding="utf-8") as f:
         return f.write(text)
