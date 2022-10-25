b5cd3607d3ae917ba40bc9483fdddadc816e2112
IVashty <vashtylavilavi50@gmail.com>
2022-10-25 20:55:47 +0000

    Task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module append_write
+It appends a string at the end of a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    adds a string at the end of a text filename
+    Arguments:
+        filename-name of file name
+        text -be appended
+    Return: The number of characters to be added
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        add_chars = f.write(text)
+        print(add_chars)
