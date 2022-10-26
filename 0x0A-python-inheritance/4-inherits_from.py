60db038b710825605924c9c9e238e30c17d25d4b
IVashty <vashtylavilavi50@gmail.com>
2022-10-26 19:53:48 +0000

    TAsk 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+"""
+Module inherits_from
+Returns a boolean if the object is an instance of a class inherited from
+specified class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    is the bj an instance of a class be inherits_from a_class
+    Arguments:
+        obj - object to liik at
+        a_class-the class to evaluate
+    Return:True or False
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
