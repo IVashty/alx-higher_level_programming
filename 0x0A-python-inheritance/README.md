729bfc2b7e0a70d5c6b318f463eab235ec594b0a
IVashty <vashtylavilavi50@gmail.com>
2022-10-26 20:29:24 +0000

    Added a few files try 1

--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -1,7 +1,36 @@
 ## TASKS:
-Write a function that returns the list of available attributes and methods of an object:
+0. Write a function that returns the list of available attributes and methods of an object:
+
 
     Prototype: def lookup(obj):
     Returns a list object
     You are not allowed to import any module
 
+1. Write a class MyList that inherits from list:
+
+    Public instance method: `def print_sorted(self):` that prints the list, but sorted (ascending sort)
+    You can assume that all the elements of the list will be of type `int`
+    You are not allowed to import any module
+2. Write a function that returns True if the object is exactly an instance of the specified class ; otherwise False.
+
+    Prototype: `def is_same_class(obj, a_class):`
+    You are not allowed to import any module
+
+3. Write a function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False.
+
+    Prototype: `def is_kind_of_class(obj, a_class):`
+    You are not allowed to import any module
+
+4. Write a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.
+
+    Prototype: `def inherits_from(obj, a_class):`
+    You are not allowed to import any module
+
+5. Write an empty class `BaseGeometry`.
+
+    You are not allowed to import any module
+
+6. Write a class `BaseGeometry` (based on 5-base_geometry.py).
+
+    Public instance method: `def area(self):` that raises an `Exception` with the message `area() is not implemented`
+    You are not allowed to import any mod
--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -17,3 +17,110 @@ class MyClass2(object):
 print(lookup(MyClass1))
 print(lookup(MyClass2))
 print(lookup(int))
+
+# TASK 1
+MyList = __import__('1-my_list').MyList
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
+
+# TASK 2
+is_same_class = __import__('2-is_same_class').is_same_class
+
+a = 1
+if is_same_class(a, int):
+    print("{} is an instance of the class {}".format(a, int.__name__))
+if is_same_class(a, float):
+    print("{} is an instance of the class {}".format(a, float.__name__))
+if is_same_class(a, object):
+    print("{} is an instance of the class {}".format(a, object.__name__))
+
+# TASK 3
+is_kind_of_class = __import__('3-is_kind_of_class').is_kind_of_class
+
+a = 1
+if is_kind_of_class(a, int):
+    print("{} comes from {}".format(a, int.__name__))
+if is_kind_of_class(a, float):
+    print("{} comes from {}".format(a, float.__name__))
+if is_kind_of_class(a, object):
+    print("{} comes from {}".format(a, object.__name__))
+
+# TASK 4
+inherits_from = __import__('4-inherits_from').inherits_from
+
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
+
+# Task 5
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
+
+# Task 6
+BaseGeometry = __import__('6-base_geometry').BaseGeometry
+
+bg = BaseGeometry()
+
+try:
+    print(bg.area())
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+# Task 7
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+bg = BaseGeometry()
+
+bg.integer_validator("my_int", 12)
+bg.integer_validator("width", 89)
+
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+# Task 8
+Rectangle = __import__('8-rectangle').Rectangle
+
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
+
+try:
+    print("Rectangle: {} - {}".format(r.width, r.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
