f5560b8327404be555b900df9acaf9dece1a9af2
IVashty <vashtylavilavi50@gmail.com>
2022-10-26 13:43:17 +0000

    Task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module 9-Student
+it creates  a Student class
+"""
+
+
+class Student:
+    """
+    Class defines a student
+    Its attributes:
+        first_name
+        last_name
+        age
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns: a dictionary representation of
+                a Student instance
+        """
+        return self.__dict__
