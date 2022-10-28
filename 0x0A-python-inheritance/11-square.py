73047b4685fa6ebc4e4e5696aa64b900dbe406b1
IVashty <vashtylavilavi50@gmail.com>
2022-10-28 14:51:24 +0000

    TAsk 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module Square.
+it uses the inherit for a new Rectangle form TASK 9
+"""
+
+
+TaSquare = __import__("10-square").Square
+
+
+class Square(TaSquare):
+    """
+    this square inherits from Square
+    """
+
+    def __init__(self, size):
+        """Constructor of square"""
+        super().__init__(size)
+
+    def __str__(self):
+        """
+        My string representation of the Square
+        """
+        return "[Square]{0:d}/{0:d}".format(self.__size)
