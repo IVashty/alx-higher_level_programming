210cbed59467b26e42c1440200d2881200f3263b
IVashty <vashtylavilavi50@gmail.com>
2022-10-31 17:31:46 +0000

    Added kwargs and args

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,19 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def update(self, *args):
+        a = ["id", "x", "y", "width", "height"]
+        for i in range(len(args)):
+            if i > len(a) - 1:
+                break
+            setattr(self, a[i], args[i])
+
+    def update2(self, *args, **kwargs):
+        b = ["id", "width", "height", "x", "y"]
+        for j in range(len(args)):
+            if j > len(b) - 1:
+                break
+            setattr(self, b[j], args[j])
+        for key, value in kwargs.items():
+            print("{} is {}".format(key, value))
