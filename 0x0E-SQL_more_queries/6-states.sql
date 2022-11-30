01d2f43d431cc5a5dcae6ac57ec5488b58879bb3
IVashty <vashtylavilavi50@gmail.com>
2022-11-30 22:08:35 +0300

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 --Create a database htbn_0d_usa
 -- Creates a table called states if it doesn't exist
 CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
+USE htbn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(id INT UNIQUE PRIMARY KEY AUTO_INCREMENT , name VARCHAR(256) NOT NULL);
