c850da84d0e2afd8fe9f99b531e2e00736183a42
IVashty <vashtylavilavi50@gmail.com>
2022-11-30 23:39:18 +0300

    13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- selects results of the search if not null and the count of each instancel
+SELECT tv_genres.name AS `genre`, COUNT(*) AS `number_shows`
+FROM tv_genres JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.show_id IS NOT NULL
+GROUP BY tv_genres.name
+ORDER BY `number_shows` DESC;
