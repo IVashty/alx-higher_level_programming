e456d16cb39b532a92f0a2ffc7ab1b20bec7dbdb
IVashty <vashtylavilavi50@gmail.com>
2022-11-30 22:22:41 +0300

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,5 +2,9 @@
 -- Creates a table called states if it doesn't EXISTS
 CREATE htbn_0d_usa;
 USE htbn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE PRIMARY KEY
-AUTO_INCREMENT NOT NULL,state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS cities(
+        id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        state_id INT NOT NULL,
+        FOREIGN KEY(state_id) REFERENCES states(id),
+        name VARCHAR(256) NOT NULL
+);
