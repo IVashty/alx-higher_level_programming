630acffe70fe4c0f1c977870a0a009007e455ee9
IVashty <vashtylavilavi50@gmail.com>
2022-11-30 18:01:04 +0300

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- create the MySQL server user  user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* to 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
