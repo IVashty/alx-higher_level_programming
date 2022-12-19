ac3013fa4dfb1477d7d947902eec6b701e38ff56
IVashty <vashtylavilavi50@gmail.com>
2022-12-19 22:36:38 +0300

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+//second largest number - algebra_precalculus
+let array = process.argv.length([]);
+for (let arrayOne = 2; arrayOne < length; arrayOne++) {
+  return (Number(process.argv[arrayOne]));
+}
+if (array.length <= 1) {
+  console.log(0);
+} else {
+  console.log(array.sort((arrayOne, arrayTwo) => { return arrayTwo - arrayOne}));
+}
