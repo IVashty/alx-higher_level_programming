4f5dd7c7099d68ee7203d91552ccfbb5b242337d
IVashty <vashtylavilavi50@gmail.com>
2022-12-19 13:05:18 +0300

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const firstLine = 'C is fun'
-const secondLine = 'Python is cool'
-const thirdLine = 'JavaScript is amazing'
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
 
-console.log(firstLine, '\n', secondLine, '\n', thirdLine)
+console.log(firstLine, '\n', secondLine, '\n', thirdLine);
