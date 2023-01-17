0e933450ff97111ffda4fa7fd62eaafcc8279fad
IVashty <vashtylavilavi50@gmail.com>
2023-01-17 23:07:51 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+fetch url and give a response status that is provided
+"""
 
 from urllib import request
 
@@ -6,5 +9,5 @@ with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     myhtml = response.read()
     print("Body response:")
     print("\t- type: {}".format(type(myhtml)))
-    print("\t- content: {}".format(type(myhtml)))
+    print("\t- content: {}".format(myhtml))
     print("\t- utf8 content:{}".format(myhtml.decode("utf-8")))
