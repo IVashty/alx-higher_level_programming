baa87ac93ed58957664cfc9a57b0f5ab76a01280
IVashty <vashtylavilavi50@gmail.com>
2023-01-19 22:06:13 +0300

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a post request with a json file
-curl -s -H POST  "Content-Type: application/json" -d "@$2"  "$1"
+curl -s -H POST  "Content-Type: application/json" -d "@$2" "$1"
