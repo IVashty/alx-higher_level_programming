c964f32bd850c626489b7eb25b3da3a2cf0e3cba
IVashty <vashtylavilavi50@gmail.com>
2023-01-29 15:09:06 +0300

    6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a peak in a list of unsorted integers.
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers.
+    """
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
