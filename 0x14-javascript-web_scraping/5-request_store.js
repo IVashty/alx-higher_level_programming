7478ad4b0980dcd2c1f2f434d59fc0bce6eedaac
IVashty <vashtylavilavi50@gmail.com>
2023-02-09 22:58:31 +0300

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
 // fs and  request are used to get the content of a webpage and the stores it in a file
-const fs = require('fs')
-const request = require('request')
+const fs = require('fs');
+const request = require('request');
 
-const webpageURL = process.argv[2]
-const fileName = process.argv[3]
+const webpageURL = process.argv[2];
+const fileName = process.argv[3];
 
 request(webpageURL, (error, response, body) => {
   if (error) {
-    console.error(error)
-    return
+    console.error(error);
+    return;
   }
 
   fs.writeFile(fileName, body, 'utf8', (err) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
     }
-  })
-})
+  });
+});
