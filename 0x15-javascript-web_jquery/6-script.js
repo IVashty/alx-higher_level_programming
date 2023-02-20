266132f160ec4b25ee3fc1fd8047261f4ef2983c
IVashty <vashtylavilavi50@gmail.com>
2023-02-20 17:24:32 +0300

    6-script.js

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// imports the jQuery library
+import $ from 'jquery';
+
+// ensures that the code runs only after the DOM is fully loaded.
+$(document).ready(function () {
+  // binds a click event to the DIV#update_header element.
+  $('#update_header').click(function () {
+    // updates the text content of the <header> element to "New Header!!!"
+    $('header').text('New Header!!!');
+  });
+});
