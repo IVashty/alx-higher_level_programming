b996377e433f9614819820ed487c9222fbfde449
IVashty <vashtylavilavi50@gmail.com>
2023-02-20 20:22:05 +0300

    ..

---
+++ 0x15-javascript-web_jquery/100-main.js
@@ -0,0 +1,7 @@
+// adds an event listener which listens for the "DOMContentLoaded" event.
+window.addEventListener('DOMContentLoaded', (event) => {
+        // Get the header element by its tag name and assign it to the 'header' variable
+        const header = document.querySelector('header');
+        // Change the 'color' style property of the header element to red
+        header.style.color = '#FF0000';
+      });
