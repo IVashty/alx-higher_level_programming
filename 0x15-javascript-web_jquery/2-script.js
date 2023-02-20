b276888020d5658a233004746f41e72847e116a1
IVashty <vashtylavilavi50@gmail.com>
2023-02-20 16:14:19 +0300

    2-script.js

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+import $ from 'jquery';
+// execute the code when the page Document Object Model (DOM) is ready.
+// a click event handler to the selected element,the function inside the curly braces {...} will be executed.
+//
+$(document).ready(function () {
+  $('#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
+});
