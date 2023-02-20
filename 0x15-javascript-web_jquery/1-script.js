901961463ba914208507256b62bf0a204493ad43
IVashty <vashtylavilavi50@gmail.com>
2023-02-20 15:53:04 +0300

    1-script.js

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// function is called when the page has finished loading.
+// It ensures that the code doesn't run until the page is ready.
+$(document).ready(function () {
+  $('header').css('color', '#FF0000');
+});
