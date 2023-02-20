21166dd92463d3032c695cb49e392a49e1d54e2e
IVashty <vashtylavilavi50@gmail.com>
2023-02-20 15:01:48 +0300

    0-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Get the header element using document.querySelector
+const header = document.querySelector('header');
+
+// Set the color property of the style object to red
+header.style.color = '#FF0000';
